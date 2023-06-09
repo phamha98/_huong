ac4c211361646c0262048f4f567b67a89bd63d07
Phạm Trần Quang Hà <phamtranquangha@gmail.com>
2023-06-10 00:58:28 +0700

    add

--- src/components/TableValidate4.tsx
+++ src/components/TableValidate4.tsx
@@ -12,8 +12,8 @@ const TableValidate4 = ({ data = [] }: any) => {
                 <Column field="x0" header="Tỉnh" align={'center'} />
                 <Column field="x1" header="Số ca nhiễm mới" align={'center'} />
                 <Column field="x2" header="Tổng số ca" align={'center'} />
-                <Column field="x3" header="Tỷ lệ ca mắc" align={'center'} />
-                <Column field="x4" header="Tỷ lệ ca mắc" align={'center'} />
+                <Column field="x3" header="Tỷ lệ ca nhiễm" align={'center'} />
+                <Column field="x4" header="Tỷ lệ ca nặng" align={'center'} />
                 <Column field="x5" header="Tỷ lệ tử vong" align={'center'} />
                 <Column field="x6" header="Tổng nhân lực y tế" align={'center'} />
                 <Column field="x7" header="TP trung ương/tỉnh" align={'center'} />
